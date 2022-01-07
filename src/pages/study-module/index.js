917968c0724a863558e0e9fc91c6f38e76f13a41
lixiaopeng <lixiaopeng06@Macs-MacBook-Pro.local>
2022-01-07 14:49:36 +0800

    fixed

--- src/pages/study-module/index.js
+++ src/pages/study-module/index.js
@@ -10,59 +10,41 @@ import {
 	Input,
 	Upload
 } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
 import api from '../../assets/api/api';
 import url from '../../assets/api/url';
 import './index.scss';
 
-const columns = [
-	{
-		title: 'ID',
-		dataIndex: 'id',
-		align: 'center'
-	},
-	{
-		title: '名称',
-		dataIndex: 'name',
-		align: 'center'
-	},
-	{
-		title: '模块图',
-		dataIndex: 'src',
-		align: 'center',
-		render: (src) => (<Image src={src} width={200} height={100} fallback="" />)
-	},
-	{
-		title: '操作',
-		align: 'center',
-		render: () => (
-			<Space>
-				<Button type="primary" size="small">编辑</Button>
-				<Button type="primary" size="small" danger ghost>删除</Button>
-			</Space>
-		)
-	}
-];
+const { Column } = Table;
+
 const StudyModule = () => {
 	const [dataList, setDataList] = useState([]);
 	const [isShow, setShow] = useState(false);
-	const [form, setForm] = useState({
-		name: 'd',
-		src: 'd',
-		tip: 'd'
-	});
-	useEffect(async () => {
+	const [form, setForm] = useState({});
+	const initData = async () => {
 		const { data } = await api.get(url.studyModule);
-		setDataList(data.map((item) => {
+		setDataList(data.map((item, key) => {
 			return {
-				key: item.id,
+				key: key + 1,
 				name: item.name,
 				src: item.src,
-				id: item.id
+				id: item.id,
+				tip: item.tip
 			};
 		}));
+	};
+	useEffect(async () => {
+		initData();
 	}, []);
-	const onAddClick = () => {
+	const onAddClick = (value) => {
 		setShow(true);
+		if (value.id) {
+			setForm({ ...form, ...value });
+		}
+	};
+	const onDelectClick = async (value) => {
+		await api.delete(`${url.studyModule}/${value.id}`);
+		initData();
 	};
 	const normFile = (e) => {
 		if (Array.isArray(e)) {
@@ -71,21 +53,17 @@ const StudyModule = () => {
 		return e?.file?.response?.data || '';
 	};
 	const handleSubmit = async (value) => {
-		await api.post(url.studyModule, value);
-		setForm({
-			name: '',
-			src: '',
-			tip: ''
-		});
+		if (form.id) {
+			value.id = form.id;
+		}
+		await api[form.id ? 'put' : 'post'](url.studyModule, value);
+		setForm({});
 		setShow(false);
+		initData();
 	};
 	const handleCancel = () => {
 		setShow(false);
-		setForm({
-			name: '',
-			src: '',
-			tip: ''
-		});
+		setForm({});
 	};
 	return (
 		<>
@@ -95,11 +73,29 @@ const StudyModule = () => {
 				</Form.Item>
 			</Form>
 			<Divider />
-			<Table bordered dataSource={dataList} columns={columns} />
+			<Table bordered dataSource={dataList}>
+				<Column title="序号" dataIndex="key" key="key" align="center" />
+				<Column title="名称" dataIndex="name" key="name" align="center" />
+				<Column title="模块图" dataIndex="src" key="src" align="center" />
+				<Column title="模块寄语" dataIndex="tip" key="tip" align="center" />
+				<Column
+					title="操作"
+					dataIndex="action"
+					key="action"
+					align="center"
+					render={(_, record) => (
+						<Space>
+							<Button type="primary" size="small" onClick={() => onAddClick(record)}>编辑</Button>
+							<Button type="primary" size="small" danger ghost onClick={() => onDelectClick(record)}>删除</Button>
+						</Space>
+					)}
+				/>
+			</Table>
 			<Modal
-				title="Modal"
+				title={form.id ? '编辑' : '添加'}
 				footer={null}
 				visible={isShow}
+				onCancel={handleCancel}
 			>
 				<Form
 					layout="horizontal"
@@ -134,7 +130,7 @@ const StudyModule = () => {
 							action="http://127.0.0.1:5000/upload"
 							listType="picture"
 						>
-							shang
+							<Button icon={<UploadOutlined />}>上传</Button>
 						</Upload>
 					</Form.Item>
 					<Form.Item
