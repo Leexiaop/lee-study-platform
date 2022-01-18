4506daa8e443eaf922793f32eec64116882d78e0
lixiaopeng <lixiaopeng06@Macs-MacBook-Pro.local>
2022-01-18 10:27:16 +0800

    提交

--- src/assets/api/url.js
+++ src/assets/api/url.js
@@ -1,20 +1,10 @@
-// const url = 'http://localweb.baidu.com:8532';
-let url = window.location.href;
-switch (process.env.NODE_ENV) {
-	case 'development':
-		url = 'http://127.0.0.1:5000';
-		break;
-	default:
-		url = `${window.location.origin}/api/`;
-}
-
 export default {
 	// 登录及用户信息相关
-	login: `${url}/login`,
-	upload: `${url}/upload`,
-	studyModule: `${url}/studyModule`,
-	question: `${url}/question`,
-	questionOnline: `${url}/question/online`,
-	answer: `${url}/answer`,
-	answerOnline: `${url}/answer/online`
+	login: '/login',
+	upload: '/upload',
+	studyModule: '/studyModule',
+	question: '/question',
+	questionOnline: '/question/online',
+	answer: '/answer',
+	answerOnline: '/answer/online'
 };
